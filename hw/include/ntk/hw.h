53464754d3bd420abea89a576686f6c1772e22ee
Tristan Ross <tross@ezoic.com>
2022-09-26 11:08:26 -0700

    feat: added better enums to hw

--- hw/include/ntk/hw.h
+++ hw/include/ntk/hw.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include "hw/error.h"
 #include "hw/device.h"
 #include "hw/display.h"
 #include "hw/input.h"
 #include <ntk/hw/build.h>
+#include <ntk/ntk-hw-enums.h>
 
 #ifdef NTK_HW_HAS_LIBDRM
 #include "hw/drm.h"
