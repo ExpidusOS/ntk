3a89a707550d332409cb79af7fb207a02569151f
Tristan Ross <tristan.ross@midstall.com>
2022-11-18 22:49:48 -0800

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -5,5 +5,5 @@
     url = github:ExpidusOS/sdk;
   };
 
-  outputs = { self, expidus-sdk }: expidus-sdk.libExpidus.mkFlake { inherit self; name = "ntk"; };
+  outputs = { self, expidus-sdk }: expidus-sdk.lib.expidus.flake.makeOverride { inherit self; name = "ntk"; };
 }
